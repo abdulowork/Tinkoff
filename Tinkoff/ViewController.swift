69c4435b1459d8a3067995c9bedee5e19d0208bb
Timofey Solonin <tim-solonin2@yandex.ru>
2017-03-20 13:45:20 +0300

    Initial commit

---
+++ Tinkoff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinkoff
+//
+//  Created by Timofey on 3/20/17.
+//  Copyright © 2017 Timofey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
